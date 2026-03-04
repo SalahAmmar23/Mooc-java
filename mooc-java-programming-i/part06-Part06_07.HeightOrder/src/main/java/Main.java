adce7ce088cf45c985af1409a49ce80f515da699
SalahAmmar <108588062+FistHell@users.noreply.github.com>
2026-03-04 14:28:35 +0200

    part 9.7 added

--- mooc-java-programming-i/part06-Part06_07.HeightOrder/src/main/java/Main.java
+++ mooc-java-programming-i/part06-Part06_07.HeightOrder/src/main/java/Main.java
@@ -10,22 +10,5 @@ public class Main {
         UserInterface.start();
         
     }
-
-
-
-     int end = word.length() - 1;
-
-        int i = 0;
-        while (i < word.length() / 2) {
-            // method charAt returns the character at given index
-            // as a simple variable
-            if(word.charAt(i) != word.charAt(end - i)) {
-                return false;
-            }
-
-            i++;
-        }
-
-        return true;
 }
 
