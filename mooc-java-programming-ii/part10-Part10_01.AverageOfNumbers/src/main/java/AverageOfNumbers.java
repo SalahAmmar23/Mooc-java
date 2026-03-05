5f87d4813d3758852abfc7824762091c92b44456
SalahAmmar <108588062+FistHell@users.noreply.github.com>
2026-03-05 22:55:06 +0200

    part 10.1 added

--- mooc-java-programming-ii/part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ mooc-java-programming-ii/part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -7,6 +7,26 @@ public class AverageOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Write your program here
+        ArrayList<String> inputs = new ArrayList<>();
+        
+        System.out.println("Input number, type \"end\" to stop.");
+
+        while(true){
+            String command = scanner.nextLine();
+            
+            if(command.equals("end")){
+                break;
+            }
+
+            inputs.add(command);
+
+
+        }
+
+        double average = inputs.stream().mapToInt(s -> Integer.valueOf(s)).average().getAsDouble();
+
+
+        System.out.println("average of the numbers: " + average);
 
     }
 }
