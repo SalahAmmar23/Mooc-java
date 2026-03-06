820559219a3a371f57f187d8783bede131d95ec9
SalahAmmar <108588062+FistHell@users.noreply.github.com>
2026-03-06 06:20:01 +0200

    part 10.5 added

--- mooc-java-programming-ii/part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ mooc-java-programming-ii/part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -7,5 +7,21 @@ public class PrintingUserInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        ArrayList<String> inputs = new ArrayList<>();
+
+        while(true){
+            String prompt = scanner.nextLine();
+
+            if(prompt.isEmpty()){
+                break;
+            }
+            inputs.add(prompt);
+        }
+
+
+
+        inputs.stream().forEach(name -> System.out.println(name));
+
+
     }
 }
