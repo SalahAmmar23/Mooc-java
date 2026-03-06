9dba30adff5729c39d026edea2da4f2b6c170656
SalahAmmar <108588062+FistHell@users.noreply.github.com>
2026-03-06 06:34:40 +0200

    part 10.6 added

--- mooc-java-programming-ii/part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ mooc-java-programming-ii/part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -1,4 +1,5 @@
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -6,6 +7,19 @@ public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> listOfNumbers = new ArrayList<>();
 
+        while(true){
+            int prompt = Integer.parseInt(scanner.nextLine());
+
+            if(prompt < 0){
+                break;
+            }
+
+            listOfNumbers.add(prompt);
+
+        }
+
+        listOfNumbers.stream().filter(s -> s >=1 && s<= 5).forEach(i -> System.out.println(i));
     }
 }
