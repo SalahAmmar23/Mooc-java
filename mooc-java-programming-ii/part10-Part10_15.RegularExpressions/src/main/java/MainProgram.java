da333773ff4fd3a9630d2a1ebb0160e1d95f8f81
SalahAmmar <108588062+FistHell@users.noreply.github.com>
2026-03-08 14:53:29 +0200

    part10-15  done

--- mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
+++ mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
@@ -1,9 +1,16 @@
 
-
 public class MainProgram {
 
     public static void main(String[] args) {
         // you can create test code here. Call the methods that you'll implement
         // during the course of this exercise
+
+            String name = "20:59:59";
+            if(name.matches("(([01][0-9])|(2[0-3])):[0-5][0-9]:[0-5][0-9]")){
+                System.out.println("right digits");
+            }else{
+                System.out.println("Not right");
+            }
+        
     }
 }
