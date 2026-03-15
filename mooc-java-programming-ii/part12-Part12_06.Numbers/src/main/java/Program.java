35248d335623663b84a6a2e0475fa289dfb9d61c
SalahAmmar <108588062+FistHell@users.noreply.github.com>
2026-03-15 16:25:30 +0200

    part12_05

--- mooc-java-programming-ii/part12-Part12_06.Numbers/src/main/java/Program.java
+++ mooc-java-programming-ii/part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,16 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("How many random numbers should be printed?");
+        int prompt = Integer.parseInt(scanner.nextLine());
+
+        Random testobject = new Random();
+        for(int i =0;  i< prompt; i++){
+            System.out.println(testobject.nextInt(11));
+        }
+
     }
+
 
 }
